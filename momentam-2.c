c8669c07661447d6d0d42d5d92a95acb2403ad44
lax ahir <laxahir88@gmail.com>
2024-03-15 11:01:12 +0530

    first

---
+++ momentam-2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+    int n;
+
+     printf("enter number");
+     scanf("%d",&n);
+
+     (n%2 == 0)?
+     (printf("%d id even",n)):(printf("%d is odd",n));
+     
+
+    return 0;
+}
---
+++ momentam-3.c
@@ -0,0 +1,13 @@
+# include <stdio.h>
+
+ int main (){
+    int a,b,c,d;
+    printf("enter number");
+    scanf("%d %d %d %d",&a,&b,&c,&d);
+
+    if ( )
+     
+
+
+    return 0;
+ }
